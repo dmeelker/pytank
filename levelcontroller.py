d675616412de78c862afd7cd983cef41eb51cdda
Dennis Meelker <dmeelker@gmail.com>
2019-11-02 15:44:05 +0100

    Restructured level controller

--- levelcontroller.py
+++ levelcontroller.py
@@ -15,27 +15,24 @@ from utilities import Vector
 from utilities import Timer
 
 tankSpawnLocations = []
+upcomingTankLevels = []
 base = None
 playerTank = None
-
+liveEnemyTanks = []
 spawnTimer = Timer(5000)
 
 def loadLevel(levelString):
-    global tankSpawnLocations, base, playerTank
+    global tankSpawnLocations, upcomingTankLevels, liveEnemyTanks, base
     entities.manager.clear()
     playfield.initialize(40, 30)
+    tankSpawnLocations = []
+    upcomingTankLevels = [0, 1, 2, 3, 4] # , 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    liveEnemyTanks = []
     resetSpawnTimer()
     
-    playerTank = entities.tank.Tank(Vector(100, 100))
-    playerTank.fireTimer.setInterval(100)
-    playerTank.movementSpeed = 3
-    playerTankController = tankcontroller.PlayerTankController(playerTank)
-    playerTank.setController(playerTankController)
-    entities.manager.add(playerTank)
-    input.tankController = playerTankController
+    recreatePlayerTank()
 
     lines = levelString.split('\n')
-    tankSpawnLocations = []
 
     for y in range(len(lines)):
         for x in range(playfield.width):
@@ -59,19 +56,63 @@ def loadLevel(levelString):
             elif character == 'S':
                 tankSpawnLocations.append(pixelLocation)
 
+def recreatePlayerTank():
+    global playerTank
+
+    playerTank = entities.tank.Tank(Vector(100, 100))
+    playerTank.fireTimer.setInterval(100)
+    playerTank.movementSpeed = 3
+    playerTankController = tankcontroller.PlayerTankController(playerTank)
+    playerTank.setController(playerTankController)
+    entities.manager.add(playerTank)
+    input.tankController = playerTankController
+
 def update(time, timePassed):
-    if spawnTimer.update(time):
-        spawnTank()
-    
+    pruneDeadEnemyTanks()
+    spawnTankIfPossible(time)
+    checkForCompletedLevel()
+    endGameIfBaseIsDestroyed()
+
+def pruneDeadEnemyTanks():
+    deadTanks = []
+    for tank in liveEnemyTanks:
+        if tank.disposed:
+            deadTanks.append(tank)
+
+    for deadTank in deadTanks:
+        liveEnemyTanks.remove(deadTank)
+
+def checkForCompletedLevel():
+    if allTanksSpawned() and not enemyTanksLeft():
+        levelCompleted()
+
+def endGameIfBaseIsDestroyed():
     if base.disposed:
         loadLevel(levels.level1)
+
+def levelCompleted():
+    loadLevel(levels.level1)
+
+def allTanksSpawned():
+    return len(upcomingTankLevels) == 0
+
+def enemyTanksLeft():
+    return len(liveEnemyTanks) > 0
 
 def resetSpawnTimer():
     spawnTimer.setInterval(random.randint(4000, 6000))
 
+def spawnTankIfPossible(time):
+    if not allTanksSpawned() and spawnTimer.update(time):
+        spawnTank()
+
 def spawnTank():
     location = getRandomTankSpawnLocation()
-    tank = tankfactory.createTank(0, location)
+    tankLevel = upcomingTankLevels.pop(0)
+    print(f'Spawning tank of level {tankLevel}')
+    tank = tankfactory.createTank(tankLevel, location)
+
+    liveEnemyTanks.append(tank)
     entities.manager.add(tank)
 
 def getRandomTankSpawnLocation():
