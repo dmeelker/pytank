76e4245a89034dbbea0652836c990f687265525f
Dennis Meelker <dmeelker@gmail.com>
2019-11-04 20:22:40 +0100

    Pathfinder!

--- game/pathfinder.py
+++ game/pathfinder.py
@@ -1,7 +1,134 @@
+import heapq
+
+class Node:
+    def __init__(self, location, distance, estimatedDistance, parent):
+        self.location = location
+        self.distance = distance
+        self.estimatedDistance = estimatedDistance
+        self.cost = distance + estimatedDistance
+        self.parent = parent
+    
+    def __lt__(self, other):
+        return self.cost < other.cost
+
+    def __str__(self):
+        return f'{self.location}, dist: {self.distance}, estdist: {self.estimatedDistance}, cost: {self.cost}'
+
+class PriorityQueue:
+    def __init__(self):
+        self.values = []
+
+    def insert(self, value):
+        print(f'Inserting {value}')
+        heapq.heappush(self.values, value)
+
+    def heapify(self):
+        heapq.heapify(self.values)
+
+    def getSmallest(self):
+        return heapq.heappop(self.values)
+
+    def isEmpty(self):
+        return len(self.values) == 0
+
+class SearchGrid:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.initialize()
+
+    def initialize(self):
+        self.data = []
+
+        for x in range(self.width):
+            row = []
+            for y in range(self.height):
+               row.append(0)
+            self.data.append(row)
+    
+    def get(self, x, y):
+        return self.data[x][y]
+
+    def set(self, x, y, value):
+        self.data[x][y] = value
+
+    def containsCoordinates(self, x, y):
+        return not (x < 0 or x >= self.width or y < 0 or y >= self.height)
+
+    def getAdjacentCellCoordinates(self, x, y):
+        adjacentCells = []
+
+        if x > 0:
+            adjacentCells.append((x-1, y))
+        if x < self.width - 1:
+            adjacentCells.append((x+1, y))
+        if y > 0:
+            adjacentCells.append((x, y-1))
+        if y < self.height - 1:
+            adjacentCells.append((x, y+1))
+
+        return adjacentCells
+
 
 class PathFinder:
     def __init__(self, searchSpace):
         self.searchSpace = searchSpace
+        self.openList = PriorityQueue()
+        self.closedList = set()
+
+    def estimateDistance(self, startLocation, endLocation):
+        x = abs(endLocation[0] - startLocation[0])
+        y = abs(endLocation[1] - startLocation[1])
+        return (x*x) + (y*y)
 
     def find(self, startLocation, endLocation):
-        return [startLocation, endLocation]
+        self.addToOpenList(Node(startLocation, 0, 0, None))
+        
+        currentNode = None
+        path = None
+
+        while not self.openList.isEmpty():
+            currentNode = self.openList.getSmallest()
+            self.closedList.add(currentNode.location)
+            
+            if currentNode.location == endLocation:
+                return self.backTrackNode(currentNode)
+
+            for adjacentCell in self.searchSpace.getAdjacentCellCoordinates(currentNode.location[0], currentNode.location[1]):
+                cellValue = self.searchSpace.get(adjacentCell[0], adjacentCell[1])
+                if adjacentCell in self.closedList or cellValue == -1:
+                    continue
+                
+                existingNode = self.findOpenNodeByLocation(adjacentCell)
+                if existingNode != None:
+                    if currentNode.cost + 1 < existingNode.cost:
+                        existingNode.parent = currentNode
+                        existingNode.distance = currentNode.distance + 1
+                        existingNode.cost = existingNode.distance + existingNode.estimatedDistance + cellValue
+                        self.openList.heapify()
+                else:
+                    estimatedDistance = self.estimateDistance(adjacentCell, endLocation)
+                    newNode = Node(adjacentCell, currentNode.distance + 1, estimatedDistance, currentNode)
+                    self.addToOpenList(Node(adjacentCell, currentNode.distance + 1, estimatedDistance, currentNode))
+                
+        return None
+
+    def backTrackNode(self, node):
+        steps = []
+        currentNode = node
+
+        while currentNode != None:
+            steps.append(currentNode.location)
+            currentNode = currentNode.parent
+        steps.reverse()
+        return steps
+
+    def addToOpenList(self, node):
+        self.openList.insert(node)
+
+    def findOpenNodeByLocation(self, location):
+        for node in self.openList.values: # Nope!
+            if node.location == location:
+                return node
+        else:
+            return None
--- tests/pathfindertest.py
+++ tests/pathfindertest.py
@@ -1,25 +1,140 @@
 import unittest
 
+from game.pathfinder import PriorityQueue
+from game.pathfinder import SearchGrid
 from game.pathfinder import PathFinder
 
+INPENETRABLE = -1
+EXTRACOST = 1
+
 class TestStringMethods(unittest.TestCase):
+    def test_startLocationIsSameAsEndLocation(self):
+        pathFinder = PathFinder(SearchGrid(5, 5))
+
+        foundPath = pathFinder.find((0,0), (0,0))
+        
+        self.assertEqual([(0,0)], foundPath)
 
     def test_freePath(self):
-        pathFinder = PathFinder([\
-            [0, 0]])
+        pathFinder = PathFinder(SearchGrid(5, 5))
 
-        foundPath = pathFinder.find((0,0), (1,1))
+        foundPath = pathFinder.find((0,0), (1,0))
         
-        self.assertEqual([(0,0), (1,1)], foundPath)
+        self.assertEqual([(0,0), (1,0)], foundPath)
 
     def test_directPathBlockedByImpenetrable(self):
-        pathFinder = PathFinder([\
-            [0, 0, 0], \
-            [0, -1, 0], \
-            [0, -1, 0]])
-        foundPath = pathFinder.find((0,1), (2,1))
+        grid = SearchGrid(5, 5)
+        grid.set(1, 0, INPENETRABLE)
+        pathFinder = PathFinder(grid)
+
+        foundPath = pathFinder.find((0,0), (2,0))
+        self.assertEqual([(0,0), (0,1), (1,1), (2,1), (2,0)], foundPath)
+
+    def test_noPath(self):
+        grid = SearchGrid(3, 1)
+        grid.set(1, 0, INPENETRABLE)
+        pathFinder = PathFinder(grid)
+
+        foundPath = pathFinder.find((0,0), (2,0))
+        self.assertEqual(None, foundPath)
+
+    def test_shortestPathGoesOverExtraCostCell(self):
+        grid = SearchGrid(5, 5)
+        grid.set(1, 0, EXTRACOST)
+        grid.set(1, 1, INPENETRABLE)
+
+        pathFinder = PathFinder(grid)
+
+        foundPath = pathFinder.find((0,0), (2,0))
         
-        self.assertEqual([(0,1), (0,0), (1,0), (2,0), (2,1)], foundPath)
+        self.assertEqual([(0,0), (1,0), (2,0)], foundPath)
+
+class TestPriorityQueue(unittest.TestCase):
+    def test_emptyQueue(self):
+        self.assertTrue(PriorityQueue().isEmpty())
+
+    def test_singleValue(self):
+        queue = PriorityQueue()
+        queue.insert((1, 'Monkey'))
+
+        smallestValue = queue.getSmallest()
+        self.assertEqual(1, smallestValue[0])
+
+    def test_multipleValues(self):
+        queue = PriorityQueue()
+        queue.insert((10, 'Item 10'))
+        queue.insert((1, 'Item 1'))
+        queue.insert((5, 'Item 5'))
+
+        self.assertEqual(1, queue.getSmallest()[0])
+        self.assertEqual(5, queue.getSmallest()[0])
+        self.assertEqual(10, queue.getSmallest()[0])
+
+    def test_remove(self):
+        queue = PriorityQueue()
+        queue.insert(10)
+        queue.insert(1)
+        queue.insert(5)
+
+        queue.remove(5)
+
+        self.assertEqual(1, queue.getSmallest())
+        self.assertEqual(10, queue.getSmallest())
+        self.assertTrue(queue.isEmpty())
+
+class TestSearchGrid(unittest.TestCase):
+    def test_sizeIsSet(self):
+        grid = SearchGrid(5, 5)
+
+        self.assertEqual(5, grid.width)
+        self.assertEqual(5, grid.height)
+
+    def test_cellsAreZeroUponInitialization(self):
+        grid = SearchGrid(5, 5)
+
+        for x in range(grid.width):
+            for y in range(grid.height):
+                self.assertEqual(0, grid.get(x, y))
+
+    def test_set(self):
+        grid = SearchGrid(5, 5)
+        grid.set(1, 1, 10)
+        self.assertEqual(10, grid.get(1, 1))
+
+    def test_containsCoordinatesOutOfBounds(self):
+        grid = SearchGrid(5, 5)
+        self.assertFalse(grid.containsCoordinates(-1, 0))
+        self.assertFalse(grid.containsCoordinates(0, -1))
+        self.assertFalse(grid.containsCoordinates(5, 0))
+        self.assertFalse(grid.containsCoordinates(0, 5))
+
+    def test_containsCoordinatesInsideBounds(self):
+        grid = SearchGrid(5, 5)
+        self.assertTrue(grid.containsCoordinates(0, 0))
+        self.assertTrue(grid.containsCoordinates(4, 4))
+
+    def test_getAdjacentCellCoordinatesMiddle(self):
+        grid = SearchGrid(5, 5)
+        result = grid.getAdjacentCellCoordinates(1, 1)
+        self.assertEqual(4, len(result))
+        self.assertTrue((0, 1) in result)
+        self.assertTrue((2, 1) in result)
+        self.assertTrue((1, 0) in result)
+        self.assertTrue((1, 2) in result)
+
+    def test_getAdjacentCellCoordinatesTopLeft(self):
+        grid = SearchGrid(5, 5)
+        result = grid.getAdjacentCellCoordinates(0, 0)
+        self.assertEqual(2, len(result))
+        self.assertTrue((1, 0) in result)
+        self.assertTrue((0, 1) in result)
+
+    def test_getAdjacentCellCoordinatesBottomRight(self):
+        grid = SearchGrid(5, 5)
+        result = grid.getAdjacentCellCoordinates(4, 4)
+        self.assertEqual(2, len(result))
+        self.assertTrue((4, 3) in result)
+        self.assertTrue((3, 4) in result)
 
 
 if __name__ == '__main__':
